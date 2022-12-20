661bb51a6f5e0f3b07a0f370c721851cc7291bdf
guille garcia <ggarciacardiel@gmail.com>
2022-12-20 09:59:54 +0100

    Iteration 2 - Create a recipe

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+mongoose.set('strictQuery', true);
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +18,12 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const newRecipe = Recipe.create({
+      title: "spaguetti with pesto", level: "Easy Peasy", ingridients: ["pasta", "pesto", "cheese"], cuisine: "Italian", dishType: "main_course", duration: "15", creator: "Luca Barilla", created: 'Thu Dec 20 1900 09:00:59 GMT+0000'
+    })
+    return newRecipe;
   })
+  .then(createdRecipe => console.log(createdRecipe))
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
